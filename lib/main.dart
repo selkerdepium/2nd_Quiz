e7ff17b43891abdfd4eb4b1c4d55f48d3e5ade70
Smalick0478 <sameermalik0478@gmail.com>
2023-05-05 16:26:13 +0500

    Splashscreen Route Added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import './quiz.dart';
 import './result.dart';
+import 'splashscreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: SplashScreen()));
 }
 
 class MyApp extends StatefulWidget {
